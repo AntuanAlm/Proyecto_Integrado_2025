8f2be3959e04f88dfecc3667cfe9dde87add7a0a
Antonio Almansa! <antonioalmansa@hotmail.com>
2025-05-27 20:16:37 +0200

    nueva_Subida_Proyecto
    ya está creada el área de los profesores, solo falta el .php para validarlos y los html para cada uno. además, he hecho una corrección de un css.

---
+++ Tecnologias/php/crear_contraseñas_profesores/crear_contraseñas_profesores.php
@@ -0,0 +1,15 @@
+<?php
+// include("C:/xampp/htdocs/Proyecto_Integrado_2025/Tecnologias/php/conexion/conexion.php");
+
+// // Generar contraseñas seguras
+// $juan_pass = password_hash("ClaveSeguraJuan1!", PASSWORD_DEFAULT);
+// $maria_pass = password_hash("ClaveFuerteMaria2@", PASSWORD_DEFAULT);
+
+// // Guardarlas en la base de datos
+// $conexion->query("UPDATE profesores SET contrasena = '$juan_pass' WHERE id = 1");
+// $conexion->query("UPDATE profesores SET contrasena = '$maria_pass' WHERE id = 2");
+
+// echo "¡Contraseñas creadas y guardadas correctamente!";
+?>
+
+<!-- he comentado este codigo para que ya no se pueda usar mas veces, lo ideal es borrarlo para que no se ejecute mas -->
